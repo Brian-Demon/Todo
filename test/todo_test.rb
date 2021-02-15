6adb2a9ff3e4bb325c05bd4f595d2b69f42c58dd
Brian-Demon <BrianWMatejka@gmail.com>
2021-02-15 13:48:57 -0600

    test add item Errno::EACCES

--- test/todo_test.rb
+++ test/todo_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "stringio"
 
 class TodoTest < Minitest::Test
   def test_that_it_has_a_version_number
@@ -9,5 +10,18 @@ class TodoTest < Minitest::Test
 
   def test_it_does_something_useful
     assert false
+  end
+
+  def test_add_adds_item
+    test_file = StringIO.new("First", "a+")
+    config = {
+      database: test_file
+    }
+    test_manager = Todo::Manager.new(config)
+    item = "Second"
+    test_manager.add(item)
+    results = test_file.rewind
+    expected = "First\nSecond\n"
+    assert_equal(expected, results)
   end
 end
