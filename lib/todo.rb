6f3a1d0c81c64c23d4cc0922fab7a878c0272939
Brian-Demon <BrianWMatejka@gmail.com>
2021-02-14 21:52:28 -0600

    Add item implementation

--- lib/todo.rb
+++ lib/todo.rb
@@ -3,6 +3,20 @@
 require_relative "todo/version"
 
 module Todo
-  class Error < StandardError; end
-  # Your code goes here...
+  class Error < StandardError
+  end
+
+  class Manager
+    def initialize(config)
+      @config = config
+    end
+
+    def database
+      @config[:database]
+    end
+
+    def add(item)
+      database.write(item, "\n")
+    end
+  end
 end
